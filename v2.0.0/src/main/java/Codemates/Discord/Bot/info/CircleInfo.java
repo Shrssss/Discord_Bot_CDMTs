251920543cc3f647028b29517f87fe8f00bd8f88
Shrssss <harunnia764@gmail.com>
2026-03-17 23:00:22 +0900

    test

--- v2.0.0/src/main/java/Codemates/Discord/Bot/info/CircleInfo.java
+++ v2.0.0/src/main/java/Codemates/Discord/Bot/info/CircleInfo.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 
+
 public class CircleInfo {
 	private String roomid="";
 	private boolean roomop=false;
